bb5d0dc81524efeb560a0a0964b8df8a65d42824
미사고-MISAGO <130230952+MISAGOKR@users.noreply.github.com>
2023-06-21 18:07:48 +0900

    Update verify.php

--- verify.php
+++ verify.php
@@ -22,3 +22,41 @@
     </div>
 </body>
 </html>
+<!-- verify.php -->
+<?php
+// 데이터베이스 연결 설정
+$host = 'localhost';
+$username = 'username';
+$password = 'password';
+$dbname = 'database';
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (!$conn) {
+    die("데이터베이스 연결 실패: " . mysqli_connect_error());
+}
+
+// 이메일 인증 처리
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $verificationCode = $_POST['verificationCode'];
+
+    // 이메일과 인증 코드를 확인하여 회원 인증 처리
+    $sql = "SELECT * FROM users WHERE email = '$email' AND verification_code = '$verificationCode'";
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) === 1) {
+        // 회원 인증 처리
+        $sql = "UPDATE users SET verified = 1 WHERE email = '$email'";
+        if (mysqli_query($conn, $sql)) {
+            echo "이메일 인증이 완료되었습니다.";
+        } else {
+            echo "이메일 인증 실패: " . mysqli_error($conn);
+        }
+    } else {
+        echo "잘못된 이메일 또는 인증 코드입니다.";
+    }
+}
+
+mysqli_close($conn);
+?>
+
