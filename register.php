51f8b8f4a0dcb8b19881dfb96e94c48f7ff3404e
미사고-MISAGO <130230952+MISAGOKR@users.noreply.github.com>
2023-06-21 18:08:45 +0900

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,29 @@
 <!-- register.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>회원가입</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>회원가입</h1>
+        <form method="POST" action="register.php">
+            <div class="form-group">
+                <label for="email">이메일</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">비밀번호</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">가입하기</button>
+        </form>
+    </div>
+</body>
+</html>
+
+<!-- register.php -->
 <?php
 // 데이터베이스 연결 설정
 $host = 'localhost';
